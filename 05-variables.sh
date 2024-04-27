b62e43da124979ae937d30f042818f0e3476241b
heman@ <hemanthmourya@gmail.com>
2024-04-27 12:18:12 +0530

    hello script

---
+++ 05-variables.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+
+echo "please enter username::"
+
+read username #here username is variable
+
+echo "please enter passward::"
+
+read passward
+
+echo "username is: $username, passward is: $passward"
