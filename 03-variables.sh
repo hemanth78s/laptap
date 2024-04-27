2991fade1b80970aef1c2590c4e8667e1427eb8e
heman@ <hemanthmourya@gmail.com>
2024-04-27 11:09:24 +0530

    hello script

---
+++ 03-variables.sh
@@ -0,0 +1,8 @@
+#!bin/bash/
+person1=ramesh #no space between = and value
+person2=suresh
+
+echo "$person1:: hello $person2 , how are you?"
+echo "$person2:: hi $person1, I am fine. how are you?"
+echo "$person1:: I am fine too. how is your work?"
+echo "$person2:: not bad. I am thinking to upgrade to devops"
